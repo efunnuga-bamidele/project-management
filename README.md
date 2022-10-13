f06baded645b85a12db3a82f1af67143e4d5c8e6
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-13 22:53:00 +0100

    Create firebase configfile, folder structure

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # React Project Management App
+This is a platform for teams to manage project task like asana
 
+
+# Backend Details
+The backend is managed on firebase  
 
---
+++ src/firebase/config.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA6vthzs68dyAg2oxjNZJ_ooBiKyqzM1AU",
+    authDomain: "theprojectworkplace.firebaseapp.com",
+    projectId: "theprojectworkplace",
+    storageBucket: "theprojectworkplace.appspot.com",
+    messagingSenderId: "860130593622",
+    appId: "1:860130593622:web:36d747810781e9c03564f7"
+  };
+
+  //initialize firebase
+
+  firebase.initializeApp(firebaseConfig)
+
+  //init services  //naming convension is optional
+  const projectFirestore = firebase.firestore()
+  const projectAuth = firebase.auth()
+
+  // timestamp
+  const timestamp = firebase.firestore.Timestamp
+
+  //things to export
+
+  export { projectFirestore, projectAuth, timestamp }
+
