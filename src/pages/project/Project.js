76a9b83ac2df257781bba3986f99734f835a5063
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-15 17:49:43 +0100

    ✨ Project summary component created

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,11 +1,29 @@
+//to extract route parameter
+import { useParams } from 'react-router-dom'
+
+//hooks
+import { useDocument } from '../../hooks/useDocument'
+
+//component
+import ProjectSummary from './ProjectSummary'
 
 // Style
 import './Project.css'
 
 export default function Project() {
+    const { id } = useParams()
+    const { document, error } = useDocument('projects', id)
+
+    if (error){
+        return <div className='error'>{error}</div> 
+    }
+    if (!document){
+        return <div className='loading'>Loading...</div>
+    }
+
     return (
-        <div>
-            Project Details
+        <div className='project-details'>
+            <ProjectSummary project ={document} />
         </div>
     )
 }
